ad6711ce87a6d5e2cd35e91a9613d697fbfe8656
Miloš Stojanović <88903071+MilosStojanovic96@users.noreply.github.com>
2021-09-07 22:13:13 +0200

    Update Resenje.sql

--- Nedelja6/Sreda/Resenje.sql
+++ Nedelja6/Sreda/Resenje.sql
@@ -1,14 +1,23 @@
+/* 1. Zadatak: */
 SELECT * FROM profesori
 WHERE godine > 45;
 
+
+/* 2. Zadatak: */
 SELECT ime, prezime FROM ucenici 
 WHERE godine > 15 AND prosek > 6.5;
 
+
+/* 3. Zadatak: */
 SELECT godine FROM ucenici
 ORDER BY godine DESC;
 
+
+/* 4. Zadatak: */
 SELECT * FROM profesori
 WHERE predmet = 'Informatika';
 
+
+/* 5. Zadatak: */
 SELECT ucenikid, ime, prezime FROM ucenici
 WHERE LENGTH (ime) = 6;
